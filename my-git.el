256355f3531335288f78bed0445bc2456e36d6ea
disbeliever <nerevar101@mail.ru>
2023-03-18 13:35:06 +0400

    Add diff-hl hooks to magit.

--- my-git.el
+++ my-git.el
@@ -1,3 +1,9 @@
 (setq magit-auto-revert-mode nil)
 
+(global-diff-hl-mode)
+(eval-after-load 'magit
+  '(progn
+     (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)))
+
 (provide 'my-git)
