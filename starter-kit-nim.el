012eddbfd4a2d04c822fc4ab97cff8c6dcfa8a7f
disbeliever <nerevar101@mail.ru>
2023-08-19 11:23:55 +0400

    Started switching to use-package: nim.

--- starter-kit-nim.el
+++ starter-kit-nim.el
@@ -1,15 +1,15 @@
-(require 'nim-mode)
+(use-package nim-mode
+  :init
+  (add-hook 'nim-mode-hook
+            (lambda ()
+              ;; (setq-local compile-command "nimble build")
+              (define-key nim-mode-map (kbd "<f8>") 'nimble--compile)
+              (define-key nim-mode-map (kbd "C-<f8>") 'nimble--run)
+              ;; (define-key nim-mode-map (kbd "M-.") 'dumb-jump-go)
+              (define-key nim-mode-map (kbd "M-.") 'nimsuggest-find-definition)
+              (diff-hl-mode)
+              )))
 
-(require 'nimble)
-
-(add-hook 'nim-mode-hook
-          (lambda ()
-            ;; (setq-local compile-command "nimble build")
-            (define-key nim-mode-map (kbd "<f8>") 'nimble--compile)
-            (define-key nim-mode-map (kbd "C-<f8>") 'nimble--run)
-            ;; (define-key nim-mode-map (kbd "M-.") 'dumb-jump-go)
-            (define-key nim-mode-map (kbd "M-.") 'nimsuggest-find-definition)
-            (diff-hl-mode)
-            ))
+(use-package nimble)
 
 (provide 'starter-kit-nim)
